ba3873f10deb3ebd10ad36dc03658acac784db11
ThatGuyAustin7 <95262660+ThatGuyAustin7@users.noreply.github.com>
2022-03-29 15:14:11 -0500

    Skeleton for connections class Skeleton for iteration 2

---
+++ Libraries/connections/connections.go
@@ -0,0 +1,30 @@
+package connections
+
+import(
+  "fmt"
+)
+
+type peerData struct
+{
+  //int
+  //string
+  //[]bytes
+}
+
+func openConn()
+{
+  //call recvThread()
+  //call sendThread()
+}
+
+func sendThread()
+{//include ip address in parameters
+  //send peerData
+}
+
+func recvThread()
+{
+  //recv peerData
+  //open new recv thread
+  //this.thread.close()
+}
