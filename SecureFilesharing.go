567dac31e78f6b6942ebf2711ac85ddd8bf569c6
Phillip Driscoll <38273962+phillipino97@users.noreply.github.com>
2022-04-17 20:22:22 -0500

    Add files via upload

--- SecureFilesharing.go
+++ SecureFilesharing.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
 	Connections "P2P-Secure-Filesharing/Libraries/connections"
@@ -15,11 +16,28 @@ func main() {
 
 	fmt.Printf("Starting Secure File Sharing Application...")
 
+	serverPtr := flag.Bool("server", false, "a bool")
+	firstPtr := flag.Bool("first", false, "a bool")
+	addPtr := flag.String("addr", "localhost", "Address")
+	portPtr := flag.String("port", "2007", "port")
+	serverportPtr := flag.String("serverport", "2007", "server port")
+
+	flag.Parse()
+
 	PeerTypes.PeerTypes()
 	Search.Search()
 	JoinNetwork.JoinNetwork("localhost")
 	Download.Download()
 	Upload.Upload()
 	Connections.Connections()
+
+	if *firstPtr {
+		Connections.RecvThread(":" + *serverportPtr)
+	} else if *serverPtr {
+		go Connections.RecvThread(":" + *serverportPtr)
+		Connections.SendThread(*addPtr, *portPtr)
+	} else {
+		Connections.SendThread(*addPtr, *portPtr)
+	}
 
 }
