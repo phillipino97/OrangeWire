8a85cbb8b821abccc45b98cf29a0e1ecf0b601b5
AlanWS2 <101482740+AlanWS2@users.noreply.github.com>
2022-04-23 18:08:48 -0500

    Update hash.go

--- Libraries/hash.go
+++ Libraries/hash.go
@@ -2,19 +2,45 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"strconv"
 
 	"golang.org/x/crypto/sha3"
-	/*
-		"os"
-		"strings"
-	*/)
+	//"strings"
+)
 
 func main() {
+	/*
+		h := sha3.New512()
+		h.Write([]byte("Enter text here to be hashed"))
+		sum1 := h.Sum(nil)
+		fmt.Printf("hash = %x\n", sum1)
+	*/
+}
 
-	h := sha3.New512()
-	h.Write([]byte("Enter text here to be hashed"))
-	sum := h.Sum(nil)
-	fmt.Printf("hash = %x\n", sum)
+func hash(filename string) {
+	fileStat, err := os.Stat(filename)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	h1 := sha3.New512()
+	h1.Write([]byte(fileStat.Name())) //hash only the file name
+	sum1 := h1.Sum(nil)
+	fmt.Printf("hash = %x\n", sum1)
+}
+
+func hash2(filename string) {
+	fileStat, err := os.Stat(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	h2 := sha3.New512()
+	h2.Write([]byte(fileStat.Name() + strconv.FormatInt(int64(fileStat.Size()), 10))) //hash file name plus its size
+	sum2 := h2.Sum(nil)
+	fmt.Printf("hash = %x\n", sum2)
 }
 
 /*
@@ -27,3 +53,4 @@ func getInfo(filename string) {
 	size, err := os.Stat(strings.TrimSpace(filename))
 }
 */
+
